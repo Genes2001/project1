40817fb758b5a0a1067ee2bd47e3b71e58f92db8
Genes2001 <91107774+Genes2001@users.noreply.github.com>
2021-10-05 09:36:42 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Yourself</title>
+    <link rel="stylesheet" href="css/loginstyle.css">
+    </style>
+</head>
+<body>
+    <div id="logo1">
+        <img src="img/logodark.svg" alt="">
+    </div>
+
+    <div class="form">
+        <form id="loginform"> 
+            <h2 style = "margin: 10px 102px;">User Login</h2>
+            <input class="inputform" type="text" placeholder="Email or Phone no.">
+            <input class="inputform" type="text" placeholder="Password">
+            <button id="btn1">Login</button>
+            <h4 style="margin: 12px 109px;">Don't have account</h4>
+            <button id="btn">Create New Account</button>
+        </form>
+    
+    </div>
+</body>
+</html>
